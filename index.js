259ede4c1dcc3b4bbc852982e37a1245ea9d2836
raspberry-pi <raspberry-pi@kakao.com>
2023-02-15 23:11:57 +0900

    Add fetchTodo

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ function onClickCreateButton() {
     todoListElement.appendChild(item);
 }
 
-function createTodoItem(title) {
+function createTodoItem(id, title, primary) {
     const item = document.createElement('div');
+    item.id = id;
     item.className = 'item';
 
     const titleElement = document.createElement('div');
@@ -22,9 +23,10 @@ function createTodoItem(title) {
 
     item.appendChild(titleElement);
     item.appendChild(removeElement);
-    
+
     const properties = {
-        priority: false
+        id: id,
+        primary: primary
     };
     removeElement.addEventListener('click', function () {
         item.remove();
@@ -32,15 +34,44 @@ function createTodoItem(title) {
     });
 
 
-    item.addEventListener('click',function(){
-        properties.priority = !!!properties.priority;
+    item.addEventListener('click', function () {
+        properties.primary = !!!properties.primary;
 
-        if(properties.priority){
+        if (properties.primary) {
             item.style.backgroundColor = '#ffffcc';
-        } else{
+        } else {
             item.style.backgroundColor = '#FFF';
         }
     });
 
     return item;
 }
+const url = 'http://10.150.36.227:8000';
+
+function fetchTodo() {
+    const listElement = document.querySelector('.todo-list');
+    fetch(`${url}/todo`, {
+        mode: 'cors'
+    }).then((res) => res.json()).then((res) => {
+        const { todos } = res;
+        for(let i = 0; i < todos.length; i++){
+            const todo = todos[i];
+            listElement.appendChild(createTodoItem(todo.id, todo.title, todo.primary))
+        }
+    });
+}
+
+
+// {
+//     method: 'POST', // *GET, POST, PUT, DELETE, etc.
+//     mode: 'cors', // no-cors, *cors, same-origin
+//     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+//     credentials: 'same-origin', // include, *same-origin, omit
+//     headers: {
+//       'Content-Type': 'application/json'
+//       // 'Content-Type': 'application/x-www-form-urlencoded',
+//     },
+//     redirect: 'follow', // manual, *follow, error
+//     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+//     body: JSON.stringify(data) // body data type must match "Content-Type" header
+//   }
